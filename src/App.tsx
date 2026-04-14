07b7b487899ae040dd5d11b159880a4a1391ff4d
ronghechen <ronghechen2027@u.northwestern.edu>
2026-04-14 00:19:29 -0500

    fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,27 @@
-const schedule = {
-  title: "CS Courses for 2018-2019",
-  courses: {
-    F101: {
-      term: "Fall",
-      number: "101",
-      meets: "MWF 11:00–11:50",
-      title: "Computer Science: Concepts, Philosophy, and Connections",
-    },
-    F110: {
-      term: "Fall",
-      number: "110",
-      meets: "MWF 10:00–10:50",
-      title: "Intro Programming for non-majors",
-    },
-    F111: {
-      term: "Fall",
-      number: "111",
-      meets: "MWF 13:00–13:50",
-      title: "Fundamentals of Computer Programming I",
-    },
-    F211: {
-      term: "Fall",
-      number: "211",
-      meets: "MWF 12:30–13:50",
-      title: "Fundamentals of Computer Programming II",
-    },
-  },
-};
+import { useState, useEffect } from 'react';
 
-const App = () => (
+
+const App = () => {
+
+  const [schedule, setSchedule] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchSchedule = async() => {
+      const response = await fetch("https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php");
+      const data = await response.json();
+      setSchedule(data);
+    };
+    fetchSchedule();
+  }, []);
+  if (!schedule) {
+    return <p>Loading...</p>;
+  }
+
+  return (
   <main className="min-h-screen bg-white p-6">
     <h1 className="mb-8 text-4xl font-bold text-black">{schedule.title}</h1>
-
     <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 xl:grid-cols-4">
-      {Object.values(schedule.courses).map((course, index) => (
+      {Object.values(schedule.courses).map((course: any, index) => (
         <div
           key={index}
           className="flex min-h-[320px] flex-col justify-between rounded-2xl border border-gray-300 bg-white p-6 shadow-sm"
@@ -55,7 +43,7 @@ const App = () => (
         </div>
       ))}
     </div>
-  </main>
-);
+  </main>);
+};
 
 export default App;
